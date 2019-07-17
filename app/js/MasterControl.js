e7de20170210bd54b86986a636f7250ba749ac43
unknown <shpoellet@gmail.com>
2019-07-17 01:22:04 -0500

    OSCserver class complete

--- app/js/MasterControl.js
+++ app/js/MasterControl.js
@@ -1,6 +1,11 @@
 'use strict'
 
- const CueList = require('./CueList.js');
-
+const CueList = require('./CueList.js');
+const OSCserver = require('./OSCserver.js');
 
 var cueList = new CueList();
+var oscServer = new OSCserver();
+
+
+oscServer.sendCommand('10.0.0.155', 7000, '/hello');
+oscServer.sendCommand('10.0.0.155', 7000, '/hello2', true, 6);
---
+++ app/js/OSCserver.js
@@ -0,0 +1,34 @@
+//OSCserver.js
+//
+//Creates an OSC instance that other objects can call to send OSC commands
+//
+//This class basically start the OSC library and provides a simple one line
+//interface for sending commands
+//
+//Last Edited By: Shawn Poellet 7-17-2019
+
+'use strict'
+
+var OSC = require('osc-js');
+
+module.exports = class OSCserver {
+  constructor(inPort){
+    this.osc = new OSC({ plugin: new OSC.DatagramPlugin() });
+    this.listenPort = inPort;
+    this.oscReady = false;
+    this.osc.on('open', () => {
+      this.oscReady = true;
+    })
+
+    //osc-js by default will bind the socket to localhost which blocks
+    //outgoing connections to other hosts.  Setting the host to '0.0.0.0'
+    //when the socket is opened will correct this issue.
+    this.osc.open({host: '0.0.0.0', port : this.listenPort})
+  }
+
+  sendCommand(host, port, command, useValue = false, value){
+    const message = new OSC.Message(command);
+    if (useValue) message.add(value);
+    this.osc.send(message, {host: host, port: port});
+  }
+}
